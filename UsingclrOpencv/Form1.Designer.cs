4df0a1625ce5c0dcfa18470550daa2180719b968
Ngô Trường Đạt <ngotruongdat381@gmail.com>
2017-04-11 12:26:54 +0700

    Very first Set up

---
+++ UsingclrOpencv/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace UsingclrOpencv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.open_btn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Image Processing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(342, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 500);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(450, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save input";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // open_btn
+            // 
+            this.open_btn.Location = new System.Drawing.Point(547, 12);
+            this.open_btn.Name = "open_btn";
+            this.open_btn.Size = new System.Drawing.Size(75, 23);
+            this.open_btn.TabIndex = 5;
+            this.open_btn.Text = "Open";
+            this.open_btn.UseVisualStyleBackColor = true;
+            this.open_btn.Click += new System.EventHandler(this.open_btn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(681, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(271, 386);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(228, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Open Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 570);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.open_btn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button open_btn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ UsingclrOpencv/Form1.cs
@@ -0,0 +1,151 @@
+﻿using MYopencv;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+
+namespace UsingclrOpencv
+{
+    public partial class Form1 : Form
+    {
+        MYGui cvGui;
+        private System.Drawing.Drawing2D.GraphicsPath mousePath;
+        Image source = null;
+        string source_path;
+        Image mainImage = null;
+        double resizeFactor;
+        double paddingVertical;
+        double paddingHorizontal;
+        private List<PointF> userInput;
+        string outputText;  //for testing only
+        string currentFileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cvGui = new MYGui();
+            textBox1.Text = "D:\\tattoo.jpg";
+            mousePath = new System.Drawing.Drawing2D.GraphicsPath();
+            userInput = new List<PointF>();
+
+        }
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            //allow user to open jpg file 
+            OpenFileDialog dlogOpen = new OpenFileDialog();
+            dlogOpen.Filter = "Jpg Files|*.jpg";
+            if (dlogOpen.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            currentFileName = dlogOpen.FileName;
+
+            //open jpg file as Bitmap
+            Bitmap img = (Bitmap)Bitmap.FromFile(dlogOpen.FileName);
+            pictureBox1.Image = img;//set picture box image to UI
+            mainImage = img;
+
+            SetupStatZoomedImage();
+            source_path = dlogOpen.FileName;
+
+        } // end of button1_click
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap result = cvGui.ImageProcessing(source_path, userInput);
+            //pictureBox1.Image = new Bitmap("yourImage.jpg"); 
+
+            pictureBox2.Image = result;//set processed image to picture box
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //int length_path = currentFileName.Length;
+            string noFile = currentFileName.Substring(Math.Max(0, currentFileName.Length - 6));
+            noFile = noFile.Substring(0, 2);
+            StreamWriter file = new System.IO.StreamWriter("D:\\605\\Source code\\UserInput\\input_" + noFile + ".txt");
+            file.WriteLine(outputText);
+            file.Close(); 
+
+        }
+
+        private void open_btn_Click(object sender, EventArgs e)
+        {
+            //allow user to open jpg file 
+            OpenFileDialog dlogOpen = new OpenFileDialog();
+            dlogOpen.Filter = "Jpg Files|*.jpg";
+            if (dlogOpen.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            //open jpg file as Bitmap
+            Bitmap img = (Bitmap)Bitmap.FromFile(dlogOpen.FileName);
+
+            pictureBox1.Image = img;//set picture box image to UI
+            mainImage = img;
+
+            SetupStatZoomedImage();
+
+            Bitmap processedImg = cvGui.ApplyFilter(img);//call opencv functions and get filterred image
+
+            pictureBox2.Image = processedImg;//set processed image to picture box
+        }
+
+        private void SetupStatZoomedImage()
+        {
+            //treat with zoom image
+            double ratio_width = (double)mainImage.Width / pictureBox1.ClientSize.Width;
+            double ratio_height = (double)mainImage.Height / pictureBox1.ClientSize.Height;
+            paddingVertical = paddingHorizontal = 0;
+
+            if (ratio_width >= ratio_height)
+            {
+                resizeFactor = ratio_width;
+                double zoomedPictureBoxHeight = (double)pictureBox1.ClientSize.Height * resizeFactor;
+                paddingVertical = (zoomedPictureBoxHeight - mainImage.Height) / 2;
+
+            }
+            else
+            {
+                resizeFactor = ratio_height;
+                double zoomedPictureBoxWidth = (double)pictureBox1.ClientSize.Width * resizeFactor;
+                paddingHorizontal = (zoomedPictureBoxWidth - mainImage.Width) / 2;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+            
+                if (pictureBox1.ClientRectangle.Contains(pictureBox1.PointToClient(Control.MousePosition)))
+                {
+                    //painting = true;
+                    Graphics g = Graphics.FromImage(mainImage);
+                    Pen pen1 = new Pen(Color.Red, 4);
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                    PointF new_point = new PointF((float)(e.X * resizeFactor - paddingHorizontal), (float)(e.Y * resizeFactor - paddingVertical));
+                    userInput.Add(new_point);
+                    g.DrawRectangle(pen1, new_point.X, new_point.Y, 3, 3);
+
+                    g.Save();
+                    pictureBox1.Image = mainImage;
+
+                    richTextBox1.Text += new_point + "\n";
+                    outputText += new_point.X + " " + new_point.Y + "\n";
+                }
+            }
+
+        }
+
+    }
+}
